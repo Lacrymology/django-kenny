50ea6c7ca313cd4f41dbe6c9834000db5ca5276e
Tomas Neme <lacrymology@gmail.com>
2014-09-19 11:30:24 +0700

    first app version

---
+++ kenny/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import TemplateView
+
+# Create your views here.
+class SongBirdView(TemplateView):
+    prefix = ""
+    def get_template_names(self):
+        path = self.kwargs['path'].rstrip('/') or 'index'
+        return [os.path.join(self.prefix, path + ".html")]
